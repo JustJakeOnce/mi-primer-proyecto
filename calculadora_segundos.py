18fb536e858c832002d20657a0748198b9a64fcd
Jacobo <jacobo.jimenez741@gmail.com>
2026-05-05 13:02:52 -0500

    adding add from 1 to 100 using for

---
+++ calculadora_segundos.py
@@ -0,0 +1,9 @@
+#este programa calcula los segundos en cierto número de horas determinadas
+
+
+horas = 2 
+seconds = 3600 # número de segundos en una hora
+
+print("Horas: ", horas) #imprime el numero de horas
+print(f'Hay {horas*seconds} segundos en {horas} horas')
+print('Adios')
