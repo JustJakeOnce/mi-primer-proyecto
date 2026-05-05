78461c13bb51ed0f0bd3b039cb93b72e4c6918fd
Jacobo <jacobo.jimenez741@gmail.com>
2026-05-05 13:39:49 -0500

    add while loop exercise

--- bucles.py
+++ bucles.py
@@ -3,3 +3,7 @@ for i in range(101):
     suma += i
 
 print(suma)
+
+contador = 0
+while contador <5:
+    print(contador)
---
+++ calculator_0.py
@@ -0,0 +1,8 @@
+num = int(input('Ingresa un número (0 para terminar): '))
+suma = 0
+while num != 0:
+    suma += num
+    num = int(input('Ingresa un número (0 para terminar): '))
+
+print(f'La suma total es: {suma}')
+
